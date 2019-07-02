127250a807cca37cffb690ed3d589596a3fe4441
asarch <asarch@hotmail.com>
2019-07-02 01:19:00 -0500

    Fixed indentation levels

--- server.lisp
+++ server.lisp
@@ -8,64 +8,81 @@
 
 (in-package :servidor)
 
-;;(start :port 5477)
+(start :port 5477)
 
-;;---------------------------------------------------------------------
-;;
-;; Dudas:
-;;
-;;---------------------------------------------------------------------
+(publish-directory :prefix "/static" :destination "./static")
+
 (publish
-	:path "/apt-cache"
-	:content-type "text/html"
-	:function
-	#'(lambda (req ent)
-		(with-http-response (req ent)
-		(with-http-body (req ent)
-			(html
-				"<!DOCTYPE html>"
-				(:html
-					(:head
-						"<meta charset='utf-8'>"
-						(:title "apt-cache online search tool"))
-				(:body
-					;; GET request
-					(:h1 "Query Name")
-					((:form :action "/apt-cache" :method :post)
-						(:p "Package name:"
-						((:input :type "text" :name "nombre"))
-						((:input :type "submit" :name "send"))))
+ :path "/apt-cache"
+ :content-type "text/html"
+ :function
+ #'(lambda (req ent)
+     (with-http-response (req ent)
+       (with-http-body (req ent)
+	 (html
+	  "<!DOCTYPE html>"
+	  (:html
+	   (:head
+	    "<meta charset='utf-8'>"
+	    "<link rel='stylesheet' type='text/css' href='/static/apt-cache.css' />"
+	    "<script type='text/javascript' src='/static/apt-cache.js'></script>"
+	    (:title "apt-cache online search tool"))
+	   (:body
+	    ;; GET request
+	    (:h1 "Query Name")
+	    ((:form :action "/apt-cache" :method :post)
+	     (:p "Package name:")
+	     ((:input :type "text" :name "nombre"))
+	     ((:input :type "submit" :name "send")))
 					
-					;; POST request
-					(when (string-equal (request-method req) "POST")
-						(let (nombre (result 0))
-							(setf nombre (cdr (assoc "nombre" (form-urlencoded-to-query (get-request-body req)) :test #'equal)))
-							(setf result (split-sequence #\newline (uiop:run-program `("apt-cache" "search" ,nombre) :output :string) :remove-empty-subseqs t))
+	    ;; POST request
+	    (when (string-equal (request-method req) "POST")
+	      (let (nombre (result 0))
+		(setf nombre (cdr (assoc "nombre" (form-urlencoded-to-query (get-request-body req)) :test #'equal)))
+		(setf result (split-sequence #\newline (uiop:run-program `("apt-cache" "search" ,nombre) :output :string) :remove-empty-subseqs t))
 							
-							(when (length result)
-								(html
-									(:h1 "Results")
-									(:p (:princ (length result) " packages found"))
+		(when (length result)
+		  (html
+		   (:h1 "Results")
 									
-									(:table
-										(:thead
-											(:tr
-												(:th ((:input :type "checkbox")))
-												(:th "Name")
-												(:th "Description")))
-										(:tbody
-											(loop for line in result while line do 
-												(html
-													(:tr
-														(:td ((:input :type "checkbox")))
+		   (:h2 "Filtering")
+		   (:p "Filter results:")
+		   ((:input :type "text" :name "filter"))
+									
+		   (:p (:princ (length result) " packages found"))
+									
+		   ((:table class "w3-table-all")
+		    (:thead
+		     (:tr
+		      (:th "#")
+		      (:th ((:input :type "checkbox")))
+		      (:th "Name")
+		      (:th "Description")))
+		    (:tbody
+		     (let ((number 0))
+		       (loop for line in result while line do
+			 (incf number)
+			 (html
+			  (:tr
+			   (:td (:princ number))
+															
+			   (let ((checkbox-id (format nil "checkbox~d" number)))
+			     (html
+			      (:td ((:input :type "checkbox" :id checkbox-id)))))
 														
-														(let ((values (split " - " line)))
-															(html
-																(:td (:princ (car values)))
-																(:td (:princ (format nil "~{~a~^ ~}" (cdr values)))))))))))
-											
-									(html
-										(:h1 "Command line")
-										(:div
-										(:pre "apt-get -y install build-essential")
-										((:input :type "button" :name "copy" :value "Copy string")))))))))))))))
+			   (let ((values (split " - " line :limit 2)))
+			     (html
+			      (:td (:princ (car values)))
+																	
+			      ;; Aqui esta el error:
+			      ;; Antes de ejecutar el script, debes de cambiar
+			      ;; el idioma de la sesion:
+			      ;;
+			      ;; export LC_ALL=en_US.UTF-8
+			      (:td (:princ (format nil "~{~a~^ ~}" (cdr values))))))))))))
+	
+		   (html
+		    (:h1 "Command line")
+		    (:div
+		     (:pre "apt-get -y install build-essential")
+		     ((:input :type "button" :name "copy" :value "Copy string")))))))))))))))
