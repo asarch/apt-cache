e7f6e815317885df754dee7f30568f6e1a0afc7d
asarch <asarch@hotmail.com>
2019-07-02 02:34:32 -0500

    Change (lenght result) to (not (zerop lenght))

--- server.lisp
+++ server.lisp
@@ -41,7 +41,7 @@
 		(setf nombre (cdr (assoc "nombre" (form-urlencoded-to-query (get-request-body req)) :test #'equal)))
 		(setf result (split-sequence #\newline (uiop:run-program `("apt-cache" "search" ,nombre) :output :string) :remove-empty-subseqs t))
 							
-		(when (length result)
+		(when (not (zerop result))
 		  (html
 		   (:h1 "Results")
 									
